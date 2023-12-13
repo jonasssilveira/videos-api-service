9a11b06f27907e144d09043046712feff630b8c6
Jonas da Silva Silveira <jonasdasilvasilveira@gmail.com>
2023-12-12 23:32:55 -0300

    chore: fixing dependencies version

--- src/main/java/com/example/vsapi/domain/entity/Video.java
+++ src/main/java/com/example/vsapi/domain/entity/Video.java
@@ -1,12 +1,12 @@
 package com.example.vsapi.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
